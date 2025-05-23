007d090d038584b17c6284edf581369199fb3702
Kaloyan Hristov <kalataegotin22@gmail.com>
2025-05-23 08:46:55 +0200

    Update FIbonacci.py

--- FIbonacci.py
+++ FIbonacci.py
@@ -1,6 +1,8 @@
-# Fibonaci chislata sa 3 , 3toto e suzdadeno ot kombinaciqta ot purvite 2 chisla
+# Fibonaci numbers are 3 in total
+# The third one is created from the sum of the first two
 num1 = 0
 num2 = 1
+# Just creating the 3rd number 
 next_number = None
 print("Let's me show you the Fibonnaci sequence!")
 loops = int(input("How many sequences do you want to see?: "))
